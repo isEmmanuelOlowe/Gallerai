ac802cecaddfa5435475b6e4e5fdf82ecae24481
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2021-11-28 15:19:52 +0000

    resonable colours set

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -6,6 +6,6 @@ interface props {
 }
 export default function Tag({tag, selected}: props) {
   return (
-    <button className={`duration-100 ease-in-out hover:scale-105 p-2 m-2 text-sm border-2 rounded-full border-yellow-700 ${selected? `bg-yellow-700 text-white`: `text-yellow-700`}`}>{tag.name}</button>
+    <button className={`duration-100 ease-in-out hover:scale-105 p-2 m-2 text-sm border-2 rounded-full border-red-400 ${selected? `bg-red-400 text-white`: `text-red-400`}`}>{tag.name}</button>
   )
 }
