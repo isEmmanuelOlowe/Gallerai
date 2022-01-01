515e169311550a4233c761c31ac102ac748f1727
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2022-01-01 17:44:05 +0000

    more changes

--- src/components/cards/SourceCard.tsx
+++ src/components/cards/SourceCard.tsx
@@ -7,8 +7,6 @@ interface props {
 }
 
 export default function SourceCard({url, source}: props) {
-
-  if (!source) {return <p>Loading...</p>}
   return (
       <>
       <Link key={url} href={`/sources/${url}`}><a className='w-full m-5 text-white card bordered bg-base-content md:w-5/12'>
