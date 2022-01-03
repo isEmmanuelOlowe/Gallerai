ce14f2bec8bb11cb644554940be761dde2870b4e
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2022-01-03 03:35:48 +0000

    new landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,20 +14,11 @@ export default function HomePage() {
         <div className='h-full bg-gradient-to-b from-base-100 via-base-100 to-transparent'>
           <Navbar/>
           <Seo/>
-          <div className='absolute inset-0 z-0 flex flex-wrap items-center min-h-screen'>
-            <div className="pt-32 text-gray-800 lg:p-0 lg:w-1/2 ">
-              <h1 className="p-5 font-serif sm:text-4xl md:text-5xl lg:text-7xl">Mathematics in the Scottish Enlightenment</h1>
-            </div>
-            {/* <div className="w-full lg:w-1/2">
-              <div className="flex items-center w-3/4 m-auto bg-local bg-top bg-cover drop-shadow-lg rounded-3xl" style={{"minHeight":"50vh","backgroundImage": "url(images/profile.jpg)"}}>
-                <div className='p-20'>
-                  <p>Welcome</p>
-                </div>
-              </div>
-            </div> */}
-          </div>
+          <main>
+            
+          </main>
         </div>
-        </div>
+      </div>
     </>
   );
 }
