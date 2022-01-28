277dede2b6501cace3ab0db5ec5135bf684c888b
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2022-01-28 10:06:58 +0000

    group hover

--- src/pages/exhibits.tsx
+++ src/pages/exhibits.tsx
@@ -28,10 +28,10 @@ export default function Exhibits() {
           exhibits.map(exhibit => {
             return (
             <Link key={exhibit.id} href={`exhibit/${exhibit.href}`}>
-              <a data-tip={exhibit.description} key={exhibit.id} className="m-5 shadow-2xl tooltip tooltip-bottom tooltip-open w-96 card">
-              <div style={{"backgroundImage":`url(${exhibit.cover})`}} className="bg-top bg-cover h-96 bg-base-300 duration-200 ease-in-out hover:scale-[1.15]">
+              <a data-tip={exhibit.description} key={exhibit.id} className="m-5 shadow-2xl group tooltip tooltip-bottom tooltip-open w-96 card">
+              <div style={{"backgroundImage":`url(${exhibit.cover})`}} className="bg-top bg-cover h-96 bg-base-300 duration-200 ease-in-out group-hover:scale-[1.15]">
                 <div className="absolute bottom-0 w-full text-white card-body">
-                  <h2 className="p-2 ease-in-out bg-opacity-50 shadow-xl card-title bg-neutral duration-400 hover:bg-opacity-90">{exhibit.name}</h2>
+                  <h2 className="p-2 ease-in-out bg-opacity-50 shadow-xl card-title bg-neutral duration-400 group-hover:bg-opacity-90">{exhibit.name}</h2>
                 </div>
               </div>
               </a>
