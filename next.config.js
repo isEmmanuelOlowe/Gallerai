ab820f5003aec13c19d8391dc4ff078bb5bb8d3b
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2022-01-28 06:26:39 +0000

    image format error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   images: {
     domains: ['api.gallerai.studio'],
-     formats: ['image/avif', 'image/webp', 'image/jpg', 'image/jpeg', "image/png"],
   },
   eslint: {
     dirs: ['src'],
