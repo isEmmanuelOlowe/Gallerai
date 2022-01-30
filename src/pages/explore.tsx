6ec01fa533a13af941d2d6e9393b55896258b61a
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2022-01-30 14:47:34 +0000

    getting hot

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,9 +1,9 @@
 import {AutoPlay,Fade,Parallax} from "@egjs/flicking-plugins";
-import Flicking from "@egjs/react-flicking";
+import Flicking, {changedEvent} from "@egjs/react-flicking";
 import { createBrowserHistory } from "history";
 import { useRouter } from 'next/router'
 import qs from "qs";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import "@egjs/flicking/dist/flicking.css";
 
@@ -20,7 +20,9 @@ interface props {
 
 export default function Explore ({pages, tagNames}: props) {
   const router = useRouter();
+  const panels = useRef();
   const plugins = [new AutoPlay({ duration: 8000, direction: "NEXT", stopOnHover: true })];
+  const [currentPanel, setCurrentPanel] = useState(0);
   const {tags} = router.query;
   const [selected, setSelected] = useState<string[]>([]);
   
@@ -58,22 +60,16 @@ export default function Explore ({pages, tagNames}: props) {
         <Seo/>
         <Flicking moveType="freeScroll" bound={true} preventClickOnDrag={false}>
           {tagNames.map(tag => {
-            if (selected.includes(tag.name)) {            
-              return (<div className="flicking-panel" key={tag.id} onClick={()=> select(tag.name)}>
-                  <Tag key={tag.id} tag={tag} selected={true}/>
-                </div>)
-              }
-            else {
-              return (<div className="flicking-panel" key={tag.id} onClick={()=> select(tag.name)}>
-                  <Tag key={tag.id} tag={tag} selected={false}/>
-                </div>)
-            }
+            return <div className="flicking-panel" key={tag.id} onClick={()=> select(tag.name)}>
+                  <Tag key={tag.id} tag={tag} selected={selected.includes(tag.name)}/>
+            </div>
             })
           }
         </Flicking>
-        <div className="flex items-center flex-wrap h-[80vh]">
-          <div className="w-screen">
-            <Flicking plugins={plugins} align={"center"} deceleration={0.05} onMove={e => {console.log(e.currentTarget)}}>
+        <div className="flex items-center flex-wrap h-[70vh] ease-in-out duration-1000">
+            <div className="hidden pt-4 pb-2 m-auto font-serif text-4xl text-center duration-200 ease-in-out md:block">{panels.current? panels.current.currentPanel? articles[panels.current.currentPanel._index]? articles[panels.current.currentPanel._index].title: articles[0].title: articles[0].title: articles[0].title}</div>
+          <div className="w-screen duration-1000 ease-in-out">
+            <Flicking plugins={plugins} align={"center"} deceleration={0.02} onChanged={(e: changedEvent) => {setCurrentPanel(e.panel._index)}} ref={panels}>
               {articles.length === 0? <h3 className="w-screen mt-10 text-center text-base-300">No Overlap of Topics</h3> : 
               articles.map(article => (<div className="odd:h-[60vh] even:h-[55vh] my-auto w-96 hover:lg:w-[26rem] ease-in-out duration-1000 hover:odd:lg:h-[61vh] hover:even:lg:h-[56vh]" key={article.id}><Card page={article}></Card></div>))}
             </Flicking>
